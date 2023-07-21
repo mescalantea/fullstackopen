e6dc932df3e3e9637f30135d5f4807d02d076f3b
Michel Escalante Álvarez <michel.escalantea@gmail.com>
2023-07-21 19:22:32 +0200

    courseinfo part2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,19 +6,7 @@ const Header = (props) => (
   <h1>{props.course}</h1>
 )
 
-const Content = (props) => (
-  <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </>
-)
+const Part = (props) => (<p>{props.part} {props.exercises}</p>)
 
 const Total = (props) => (<p>Number of exercises {props.total}</p>)
 
@@ -35,7 +23,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
