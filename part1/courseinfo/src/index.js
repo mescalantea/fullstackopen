362d87689f94aa57c3eda288934de5a783ce7aa4
Michel Escalante Álvarez <michel.escalantea@gmail.com>
2023-07-21 20:37:59 +0200

    courseinfo part4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -13,32 +13,34 @@ const Total = (props) => (<p>Number of exercises {props.total}</p>)
 const App = () => {
 
   const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  let total = 0
 
   return (
     <div>
       <Header course={course} />
       <div>
-        <Part part={part1.name} exercises={part1.exercises} />
-        <Part part={part2.name} exercises={part2.exercises} />
-        <Part part={part3.name} exercises={part3.exercises} />
+        {parts.map(({ name, exercises }) => {
+          total += exercises
+          return <Part part={name} exercises={exercises} />
+        })
+        }
       </div>
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Total total={total} />
     </div>
   )
 }
