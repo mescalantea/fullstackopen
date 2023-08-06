0b5ae26d4e496c72db2fb0e89cd0a77931dd86b4
Michel Escalante Álvarez <michel.escalantea@gmail.com>
2023-08-06 22:58:47 +0200

    part3 ex3.11

---
+++ part2/phonebook/build/precache-manifest.9cf7741c79c37f80c70d427d5a07eb0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bdc828ad1dbe9d4304d",
+    "url": "/static/css/main.74b95a43.chunk.css"
+  },
+  {
+    "revision": "1bdc828ad1dbe9d4304d",
+    "url": "/static/js/main.1bdc828a.chunk.js"
+  },
+  {
+    "revision": "5f52602ced126a810806",
+    "url": "/static/css/1.bb421ace.chunk.css"
+  },
+  {
+    "revision": "5f52602ced126a810806",
+    "url": "/static/js/1.5f52602c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c15b7445457ae3308bacd776da4f522f",
+    "url": "/index.html"
+  }
+];
